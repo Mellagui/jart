42b04dd665f99b4d7a1fff06f512febc0680fc35
someone <simonbakhcha@gmail.com>
2025-11-09 04:55:51 +0100

    square for me to work on cube

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Image image = new Image(1000, 1000);
 
+        // Draw some points
         Point point1 = new Point(50, 100);
         point1.draw(image);
 
@@ -13,6 +14,35 @@ public class Main {
         Point point3 = new Point(500, 600);
         point3.draw(image);
 
+        // Draw a square with 45-degree rotation
+        Square square1 = new Square(new Point(200, 200), 100, 45);
+        square1.draw(image);
+
+        // Draw a square with no rotation
+        Square square2 = new Square(new Point(500, 200), 80, 0);
+        square2.draw(image);
+
+        // Draw a pentagon pointing upward (default)
+        Pentagon pentagon1 = new Pentagon(new Point(200, 500), 80);
+        pentagon1.draw(image);
+
+        // Draw a pentagon with 36-degree rotation
+        Pentagon pentagon2 = new Pentagon(new Point(500, 500), 90, 36);
+        pentagon2.draw(image);
+
+        // Draw random squares with random rotations
+        for (int i = 0; i < 10; i++) {
+            Square randomSquare = Square.random(image.getWidth(), image.getHeight());
+            randomSquare.draw(image);
+        }
+
+        // Draw random pentagons with random rotations
+        for (int i = 0; i < 10; i++) {
+            Pentagon randomPentagon = Pentagon.random(image.getWidth(), image.getHeight());
+            randomPentagon.draw(image);
+        }
+
+        // Uncomment these to test other shapes
         // Line line = new Line(new Point(50, 50), new Point(300, 300));
         // line.draw(image);
 
